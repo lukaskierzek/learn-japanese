7ceb7d15396e3978f8567e77c7c60c02984c1316
lukaskierzek <lukas.kierzek@gmail.com>
2022-12-02 21:41:37 +0100

    Modify models.py
    Add Words class with properties:
    - word_in_japanese,
    - translate_word,
    - sentence_in_japanese,
    - sentence,
    - part_of_speech,
    - last_update,
    and add Meta class with ordering property

--- api/models.py
+++ api/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Words(models.Model):
+    word_in_japanese = models.CharField(
+        max_length=50,
+        help_text="Enter a word in japanese",
+        verbose_name='Japanese word'
+    )
+
+    translate_word = models.CharField(
+        max_length=50,
+        help_text="Enter the word translation",
+        verbose_name="Word translated"
+    )
+
+    sentence_in_japanese = models.CharField(
+        max_length=150,
+        help_text="Enter a sentence in japanese",
+        verbose_name="Japanese sentence",
+        blank=True
+    )
+
+    sentence = models.CharField(
+        max_length=250,
+        help_text="Enter translation",
+        verbose_name="Japanese the sentence translation",
+        blank=True
+    )
+
+    ADJECTIVE = 'A'
+    INTERJECTION = 'I'
+    NOUN = 'N'
+    PHRASE = 'P'
+    QUANTIFIER = 'Q'
+    VERB = 'V'
+    IDK = 'IDK'
+    PARTH_OF_SPEECH_CATEGORIES = [
+        (ADJECTIVE, "Adjective"),
+        (INTERJECTION, "Interjection"),
+        (NOUN, "Noun"),
+        (PHRASE, "Phrase"),
+        (QUANTIFIER, "Quantifier"),
+        (VERB, "Verb"),
+        (NOUN, "Noun"),
+        (IDK, "IDK"),
+    ]
+    part_of_speech = models.CharField(
+        max_length=3,
+        choices=PARTH_OF_SPEECH_CATEGORIES,
+        default=IDK
+    )
+
+    last_update = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['part_of_speech']
