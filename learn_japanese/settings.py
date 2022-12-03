146ab771de7a24b54c55f4e527d8ef29c7835fbc
lukaskierzek <lukas.kierzek@gmail.com>
2022-12-03 21:09:47 +0100

    Modify settings.py
    Add connection information for PostgreSQL

--- learn_japanese/settings.py
+++ learn_japanese/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'learn_japanese.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'learn-japanese',
+        'USER': 'postgres',
+        'PASSWORD': '12qw#$ER',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
