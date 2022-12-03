272adb6c3c944e2b86877974ef912ecefae0ce4c
lukaskierzek <lukas.kierzek@gmail.com>
2022-12-03 21:45:00 +0100

    Modify admin.py
    Add WordsAdmin class
    Add list_display property
    Add list_filter property
    Add search_fields property

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Words
+
 
 # Register your models here.
+
+@admin.register(Words)
+class WordsAdmin(admin.ModelAdmin):
+    list_display = (
+        "word_in_japanese",
+        "translate_word",
+        "sentence_in_japanese",
+        "sentence",
+        "part_of_speech",
+        "last_update",
+    )
+
+    list_filter = (
+        "part_of_speech",
+    )
+
+    search_fields = [
+        "word_in_japanese",
+        "part_of_speech",
+    ]
