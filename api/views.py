ff0bf14f7ece9c7575be32287f4ea4cc2f79141e
lukaskierzek <lukas.kierzek@gmail.com>
2022-12-04 14:03:06 +0100

    Modify api.views.py
    Add WordsViewSet class with viewsets.ModelViewSet parametr

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import WordsSerializer
+from .models import Words
+
 
 # Create your views here.
+class WordsViewSet(viewsets.ModelViewSet):
+    queryset = Words.objects.all()
+    serializer_class = WordsSerializer
